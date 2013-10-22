e48be096e9aef93f38aa17e5bac663055631dc27
Stephen Mason <stevefloat@Stephens-MacBook-Air.local>
2013-10-21 22:51:58 -0400

    got rid of highlightjs, using pygmentizr or whatevr.

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@
 var fs = require('fs');
 var marked = require('marked');
 var pygmentize = require('pygmentize-bundled');
-var highlight = require('highlight.js');
 
 /*
  * GET home page.
@@ -26,8 +25,11 @@ var highlight = require('highlight.js');
 // };
 
 var markedOps = {
-	highlight: function (code, lang) {
-		return highlight.highlightAuto(lang, code).value;
+	highlight: function (code, lang, callback) {
+		pygmentize({ lang: lang, format: 'html' }, code, function (err, result) {
+			if (err) return callback(err);
+			callback(null, result.toString());
+		});
 	},
 	tables: true,
 	smartLists: true,
@@ -39,36 +41,30 @@ var definitionList = {
 	dd : /(?:(^:\s)(.*(?=<\/p>)|.*$)(<\/p>)?)/gm,
 	dl : /((?:^(<dt).*>[\n|\r]+|(?:<dd>.*<\/dd>)[\n\r]+)+(?![\r\n\S]*<dt>|[\r\n\S]+<dd>))/gm
 };
-var refLinkRegex = /^\[(.+)\]:\s(.+)(?=\s"(.+)"$|\s?$)/gm;
+var refLinkRegex = /^(?!!|\[!)\[(.+)\]:\s(.+)(?=\s"(.+)"$|\s?$)/gm;
 var md;
 var file = "/Users/stevefloat/Documents/notes/car-computers.md";
+//var file = "/Users/stevefloat/git/merk/public/bower_components/pure/README.md";
 
 exports.index = function(req, res){
 	fs.readFile(file, {encoding: "utf8"}, function(err, fileOutput){ 
 		if (err) errorHandler(err);
+		//var preformat = fileOutput;
 		var preformat = fileOutput.replace(refLinkRegex, function($0, $1, $2, $3){
-			console.log('preformatting');
-			// $0	Whole line
-			// $1	Reference Text
-			// $2	URL (not verified, use relative links or whatever)
-			// $3	Title text
-			var title = $3 ? $3 : $1; // Set Title to label if there is no alt specified
+			var title = $3 ? $3 : $1;
 			var referenceLink = '<a href="'+$2+'" title="'+title+'" target="_blank">'+$1+'</a>';
-			var referenceString = $0+'\n'+referenceLink;
-			//console.log("$3 "+$3+";\n$2 "+$2+";\n$1 "+$1+";\n$0 "+$0+";");
-			return referenceString;
+			return $0+'\n'+referenceLink;
+		}).replace(definitionList.dt, function($0, $1, $2){
+			return '<dt>'+$2+'</dt>';
+		}).replace(definitionList.dd, function($0, $1, $2){
+			return '<dd>'+$2+'</dd>';
+		}).replace(definitionList.dl, function($0, $1){
+			return '<dl>\n'+$1+'</dl>\n\n';
 		});
 		marked(preformat, markedOps, function (err, content) {
 			if (err) errorHandler(err);
-			console.log("markdown conversion successful");
-			var formatted = content.replace(definitionList.dt, function($0, $1, $2){
-										return '<dt>'+$2+'</dt>';
-									}).replace(definitionList.dd, function($0, $1, $2){
-										return '<dd>'+$2+'</dd>';
-									}).replace(definitionList.dl, function($0, $1){
-										return '<dl>\n'+$1+'</dl>\n\n';
-									});
-			res.render("index", { title: "Markdown Viewer", md: formatted});
+			var formatted = content;
+			res.render("index", { title: "merk", md: formatted});
 		});
 	});
 
